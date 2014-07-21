49021843b7805ea669a9ee7d2fd57e61270a3c44
Todd Treece <toddtreece@gmail.com>
2014-07-21 16:53:06 -0400

    adds config client js

---
+++ public/js/src/config.js
@@ -0,0 +1,195 @@
+(function($) {
+
+  var templates = {},
+      config = {};
+
+  var defaults = {
+    input: {
+      name: 'phant-input-http',
+      phantConfig: {
+        name: 'HTTP',
+        options: [
+          {
+            "label": "Metadata",
+            "name": "metadata",
+            "default": "phant-meta-nedb",
+            "type": "select",
+            "require": "meta",
+            "description": "The phant metadata module to use"
+          },
+          {
+            "label": "Keychain",
+            "name": "keychain",
+            "default": "phant-keychain-hex",
+            "type": "select",
+            "require": "keychain",
+            "description": "The phant keychain module to use"
+          },
+          {
+            "label": "Validator",
+            "name": "validator",
+            "default": "phant-validator-default",
+            "type": "select",
+            "require": "validator",
+            "description": "The phant validator module to use"
+          }
+        ]
+      }
+    },
+    output: {
+      name: 'phant-output-http',
+      phantConfig: {
+        name: 'HTTP',
+        options: [
+          {
+            "label": "Storage",
+            "name": "strorage",
+            "default": "phant-stream-csv",
+            "type": "select",
+            "require": "stream",
+            "description": "The phant stream storage module to use"
+          },
+          {
+            "label": "Keychain",
+            "name": "keychain",
+            "default": "phant-keychain-hex",
+            "type": "select",
+            "require": "keychain",
+            "description": "The phant keychain module to use"
+          },
+          {
+            "label": "Validator",
+            "name": "validator",
+            "default": "phant-validator-default",
+            "type": "select",
+            "require": "validator",
+            "description": "The phant validator module to use"
+          }
+        ]
+      }
+    },
+    manager: {
+      name: 'phant-manager-telnet',
+      phantConfig: {
+        name: 'Telnet',
+        options: [
+          {
+            "label": "Port",
+            "name": "port",
+            "default": "8081",
+            "type": "number",
+            "description": "The TCP port to listen on."
+          },
+          {
+            "label": "Metadata",
+            "name": "metadata",
+            "default": "phant-meta-nedb",
+            "type": "select",
+            "require": "meta",
+            "description": "The phant metadata module to use"
+          },
+          {
+            "label": "Keychain",
+            "name": "keychain",
+            "default": "phant-keychain-hex",
+            "type": "select",
+            "require": "keychain",
+            "description": "The phant keychain module to use"
+          },
+          {
+            "label": "Validator",
+            "name": "validator",
+            "default": "phant-validator-default",
+            "type": "select",
+            "require": "validator",
+            "description": "The phant validator module to use"
+          }
+        ]
+      }
+    }
+  };
+
+  config.loadTemplates = function(el) {
+
+    var promises = [];
+
+    el.find('[type="text/x-handlebars-template"]').each(function(i, v) {
+
+      var url = $(v).attr('src'),
+          name = url.match(/([^\/]+)(?=\.\w+$)/)[0];
+
+      promises.push($.get(url, function(data) {
+        templates[name] = Handlebars.compile(data);
+      }));
+
+    });
+
+    return promises;
+
+  };
+
+  config.renderForm = function(el) {
+
+    var input = el.find('[name=input]'),
+        output = el.find('[name=output]'),
+        manager = el.find('[name=manager]'),
+        def;
+
+    // default input
+    def = $(templates.option(defaults.input));
+    def.data('package', {http: true, included: 'HttpInput'});
+    input.append(def);
+
+    // default output
+    def = $(templates.option(defaults.output));
+    def.data('package', {http: true, included: 'HttpOutput'});
+    output.append(def);
+
+    // default manager
+    def = $(templates.option(defaults.manager));
+    def.data('package', {included: 'TelnetManager'});
+    manager.append(def);
+
+    $.each(el.data('packages'), function(i, p) {
+
+      var option;
+
+      if(! p.phantConfig) {
+        return;
+      }
+
+      if(/^phant-input/.test(p.name)) {
+        option = $(templates.option(p));
+        option.data('package', p);
+        return input.append(option);
+      }
+
+      if(/^phant-output/.test(p.name)) {
+        option = $(templates.option(p));
+        option.data('package', p);
+        return output.append(option);
+      }
+
+      if(/^phant-manager/.test(p.name)) {
+        option = $(templates.option(p));
+        option.data('package', p);
+        return manager.append(option);
+      }
+
+    });
+
+  };
+
+  $.fn.configurator = function() {
+
+    var promises = config.loadTemplates(this),
+        el = this;
+
+    $.when.apply(this, promises).done(function() {
+      config.renderForm(el);
+    });
+
+  };
+
+}(jQuery));
+
