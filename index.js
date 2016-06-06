19c764581a0b4215e5ef4e10f19cd556af08e712
rberlia <rishabh.berlia@sparkfun.com>
2016-06-06 13:26:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,8 @@ app.expressInit = function() {
       } // 1 month
     )
   );
-
+  
+  exp.use('/ifttt/v1/status', ifttt-status)
   exp.use(express.compress());
   exp.use(bodyParser.json());
   exp.use(bodyParser.urlencoded({
@@ -176,7 +177,7 @@ app.expressInit = function() {
 
   });
   
-  exp.post('/iftt/v1/test/setup', ifttt.status);
+  exp.post('/iftt/v1/test/setup', ifttt-status);
 
 
   //exp.post('/config/publish', config.publishPackage);
